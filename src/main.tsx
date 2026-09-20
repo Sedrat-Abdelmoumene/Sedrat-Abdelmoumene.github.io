1084e38e03569694f8ea4b6cd54ff46ea5aea114
Sedrat Abdelmoumene <abdelmoumenesedrat@gmail.com>
2026-09-20 15:19:22 -0700

    Add subtle animated background effects

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App'
 import './redesign.css'
 import './theme-fixes.css'
 import './portrait-modal.css'
+import './background-effects.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
